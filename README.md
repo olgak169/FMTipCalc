deffac6c6de76a6a959e5c9a623c8321621685ea
olgak169 <okrupnova@gmail.com>
2021-07-22 18:04:46 +0600

    done

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/olgak169/FMTipCalc/)
+- Live Site URL: [Add live site URL here](https://olgak169.github.io/FMTipCalc/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS
+- JS
+
+### What I learned
+
+...
+
+### Continued development
+
+Drag and drop needs some work
+
+### Useful resources
+
+...
+
+## Author
+
+
+- Frontend Mentor - [@olgak169](https://www.frontendmentor.io/profile/olgak169)
+
+
+## Acknowledgments
+
+Good challenge! Thanks to Frontend Mentor for the oportunity.
---
+++ script.js
@@ -0,0 +1,101 @@
+let helper = document.getElementById('helper')
+window.addEventListener('resize', () => {
+    helper.innerText= window.innerWidth
+})
+const pplInput = document.getElementById('num-people')
+const billInput = document.getElementById('bill-amount')
+const customInput = document.getElementById('tip-custom')
+const tipInputs = document.querySelectorAll('.tip input[type = "radio"]')
+
+const tipAmountOutput = document.getElementById('tip-person')
+const totalOutput = document.getElementById('tip-total')
+const reset = document.getElementById('btn-reset')
+
+let pplNum = ''
+let billTotal =''
+let tipSelected = ''
+reset.disabled = true
+
+reset.addEventListener('click', resetAll)
+pplInput.addEventListener('input', () => {
+    getInputs(pplInput)
+})
+billInput.addEventListener('input', () => {
+    getInputs(billInput)
+})
+customInput.addEventListener('input', () => {
+    radioInactive()
+    getInputs(customInput)
+})
+
+tipInputs.forEach(inp => {
+    inp.addEventListener('click' , () => {
+        radioInactive()
+        getInputs(inp)
+    })
+    
+})
+function getInputs(el) {
+    if (el.validity.valid) {
+        if(el.id === 'bill-amount'){
+            billTotal = el.value
+        } else if (el.id === 'num-people') {
+            pplNum = el.value 
+        } else if (el.id === 'tip-custom') {
+            tipSelected = customInput.value
+        } else if(el.name === 'tip'){
+            el.parentElement.classList.add('active')
+            customInput.value = ''
+            tipSelected = el.value
+        }
+        output()
+        resetReady()
+    }
+}
+function radioInactive() {
+    tipInputs.forEach(inp => {
+        inp.parentElement.classList.remove('active')
+    })
+}
+function output() {
+    if (pplNum != 0 && billTotal != 0 && tipSelected != '') {
+        let tipPercent = (billTotal / 100) * tipSelected
+        let tipPerPerson = tipPercent / pplNum
+        let totalPerPerson = (billTotal / pplNum ) + tipPerPerson
+
+        totalOutput.innerHTML = '$' + totalPerPerson.toFixed(2)
+        tipAmountOutput.innerHTML = animateValue(tipAmountOutput, tipPerPerson)
+        reset.classList.add('ready')
+    }
+}
+function resetReady() {
+    reset.disabled =false
+    reset.classList.add('ready')
+}
+function animateValue(elem, val) {
+    let start = 0;
+    let increment = 0.2
+    let timer = setInterval(() => {
+        if (start >= val) {
+            elem.innerHTML = '$' + val.toFixed(2);
+            clearInterval(timer);
+        } else {
+            start += increment;
+            elem.innerHTML = '$' + start.toFixed(2);
+        }
+    }, 1);
+}
+function resetAll() {
+    pplNum = ''
+    billTotal =''
+    tipSelected = ''
+    radioInactive()
+    pplInput.value = pplNum
+    billInput.value = billTotal
+    customInput.value = tipSelected
+    totalOutput.innerHTML = '$0.00'
+    tipAmountOutput.innerHTML = '$0.00'
+    reset.classList.remove('ready')
+    reset.disabled = true
+}
+resetAll()
